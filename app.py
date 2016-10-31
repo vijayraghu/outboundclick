8657d9653f0031efd943c7dba0e40d0e80e02184
vijayraghu <vijayasarathypr@gmail.com>
2016-10-31 18:52:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ client =  TwilioRestClient(os.environ.get('TWILIO_ACCOUNT_SID'), os.environ.get(
 # Entry to outbound from pythontwil
 @app.route('/outbound', methods=['POST'])
 def outbound():
-  response = twiml.Response()
-  response.say("Please hold on while we connect you to an agent")
-  with response.dial() as dial:
-    dial.number(os.environ.get('DNIS_NUMBER'))
-  return str(response)
+	response = twiml.Response()
+	response.say("Please hold on while we connect you to an agent")
+	with response.dial() as dial:
+		dial.number(os.environ.get('DNIS_NUMBER'))
+		return str(response)
 			
 if __name__ == '__main__':
 	port = int(os.getenv('PORT', 5000))
